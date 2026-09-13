5355453f9e9508d0a87e90ff2caca55074868e53
MirzaolimTuhtasinov <mirzaolimtoxtasinov8@gmail.com>
2026-09-13 16:36:36 +0900

    feat :learn python objects

---
+++ objects.py
@@ -0,0 +1,27 @@
+''' OBJECTS
+    (1) What is the object
+    (2) Iterable objects & RANGE
+    (3) DICTIONARY
+    (4) Error handling system
+'''
+
+import array  # package/module
+import math
+from math import ceil, asin
+print("=== What is the object ===")
+# An object has state and method properties.
+# Everything is object in Python!
+
+print(type("Hello World!"))
+print(type(222))
+print(type(True))
+print(type(array))
+print(type(math))
+
+# Paradigms > Functional Programmming & OOP
+# OOP 4 CONCEPTS > Abstaction | Encapsulation | Inheritence | Polymorphism
+result1 = math.ceil(97.7)  # CALL
+print("result1:", result1)
+
+result2 = ceil(99.3)
+print("result2:", result2)
